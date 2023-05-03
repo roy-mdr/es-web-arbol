517cf053fff64c0d8b028df354c92eccb1716bbf
Roy Mdr <go_rojo@hotmail.com>
2023-05-02 19:57:49 -0600

    Move select-item-by-id method to MainTree store

--- src/lib/components/EditSelected.svelte
+++ src/lib/components/EditSelected.svelte
@@ -5,53 +5,16 @@
 	import { selectedId } from '$lib/stores/appState';
 	import { speedMs } from '$lib/stores/appConstants';
 
-	let parentRoute: App.Zone['route'] | undefined;
-	let thisIndex: number | undefined;
 	let inputName: HTMLElement;
 	let itemData: App.Zone | App.Activity | undefined;
 	let itemClone: App.Zone | App.Activity | undefined;
 
 	let setColor = false;
 
-	function getSelected(rootItem: App.Zone | App.Activity, id: App.Zone['id'] | App.Activity['id']) {
-		parentRoute = undefined;
-		thisIndex = undefined;
-		itemData = selectById(rootItem, id);
+	function getSelected(trigger: any, id: App.Zone['id'] | App.Activity['id']) {
+		itemData = mainTree.selectById(id);
 		itemClone = structuredClone(itemData);
 		setColor = itemClone?.type == 'zone' ? (itemClone?.color ? true : false) : false;
-	}
-
-	function selectById(
-		rootItem: App.Zone | App.Activity,
-		id: App.Zone['id'] | App.Activity['id']
-	): App.Zone | App.Activity | undefined {
-		if (rootItem.id == id) {
-			let thisRoute = rootItem.route.split('/') || '';
-			thisIndex = parseInt(<string>thisRoute?.pop()) || 0;
-			parentRoute = thisRoute.join('/');
-
-			return rootItem;
-		} else {
-			if (rootItem.type == 'zone') {
-				// Use a for loop instead of forEach to avoid nested functions
-				// Otherwise "return" will not work properly
-				for (let i = 0; i < rootItem.children.length; i++) {
-					// Search in the current child
-					let innerSearch = selectById(rootItem.children[i], id);
-
-					if (innerSearch) {
-						let thisRoute = innerSearch.route.split('/') || '';
-						thisIndex = parseInt(<string>thisRoute?.pop()) || 0;
-						parentRoute = thisRoute.join('/');
-
-						return innerSearch;
-					}
-				}
-			}
-
-			// The node has not been found and we have no more options
-			return;
-		}
 	}
 
 	function focusInput(trigger: any) {
@@ -62,6 +25,8 @@
 	$: focusInput($selectedId);
 
 	function duplicateItem() {
+		const { parentRoute, thisIndex } = getIndex();
+
 		mainTree.copyItem({
 			from_list: parentRoute || '',
 			from_index: thisIndex || 0,
@@ -70,13 +35,29 @@
 		});
 	}
 
+	function deleteItem() {
+		const { parentRoute, thisIndex } = getIndex();
+		mainTree.deleteItem(parentRoute || '', thisIndex || 0);
+	}
+
+	function getIndex() {
+		let thisRoute = itemClone?.route.split('/') || [];
+		let thisIndex = parseInt(<string>thisRoute?.pop()) || 0;
+		let parentRoute = thisRoute.join('/');
+		return {
+			thisRoute: thisRoute,
+			thisIndex: thisIndex,
+			parentRoute: parentRoute
+		};
+	}
+
 	function submitChanges() {
 		for (const key in itemClone) {
 			// @ts-ignore --- tu q vasa ber d la bida mijo
 			itemData[key] = itemClone[key];
 		}
 		if (!setColor && itemData?.type == 'zone') {
-			delete itemData?.color;
+			delete itemData.color;
 		}
 		mainTree.rebuild();
 		selectedId.set('');
@@ -111,7 +92,7 @@
 				{/if}
 			{/if}
 
-			{#if itemClone?.type == 'act'}
+			{#if itemClone.type == 'act'}
 				<label>
 					Area:
 					<input type="text" bind:value={itemClone.area} />
@@ -122,11 +103,8 @@
 				<button type="submit">Save</button>
 				<button type="button" on:click={duplicateItem}>Duplicate</button>
 				<button type="button" on:click={() => selectedId.set('')}>Cancel</button>
-				{#if parentRoute}
-					<button
-						type="button"
-						on:click={() => mainTree.deleteItem(parentRoute || '', thisIndex || 0)}>Delete</button
-					>
+				{#if itemClone.route && itemClone.route !== '0'}
+					<button type="button" on:click={deleteItem}>Delete</button>
 				{/if}
 			</div>
 		</form>
